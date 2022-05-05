2901b5f23805f4d3cd3143e5c032124b6ed62725
Inoubli-Htm <inoubli.h@gmail.com>
2022-05-05 12:14:02 +0100

    projet react 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import img from './image/img.jpg';
+import video from './video/video.mp4';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style ={{border:"solid 1px black", width:'100vw'}}>
+          <img src={"/img.jpg"} className = "my-profile" alt="my profile"/>
+            <br/>
+              <h1 className="title red">Inoubli Haythem</h1>
+            <br/>
+            <div className="img-Container">
+              <img src={img} className= "cover-photo" alt= "cover"/>     
+            </div>
+            
+            <video className="Video-container"width={320} height={240} controls autostart autoplay>
+              <source  type={"video/mp4"} src={video}/> 
+            </video>
+      </div>
     </div>
   );
 }
